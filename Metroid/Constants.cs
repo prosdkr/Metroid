34fb3f6b28f104f2c43feb9e471b9436f39e97a2
Isam Hashmi <isam@techrxservices.com>
2018-09-20 00:57:43 -0500

    Animation, game constants file.

---
+++ Metroid/Constants.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+
+
+namespace Metroid
+{
+    public class Constants
+    {
+        public const int TILE_WIDTH = 32;
+        public const int TILE_HEIGHT = 32;
+
+        public const int SAMUS_STANDING_HEIGHT = 43;
+        public const int SAMUS_STANDING_WIDTH = 35;
+
+        public const double Rad2Deg = 180.0 / Math.PI;
+
+
+        public const int LEFT_RUN_STATES = 10;
+
+        public static int State = 0;
+
+
+
+        public static int SamusPositionX { get; set; } = 0;
+        public static int SamusPositionY { get; set; } = 0;
+        public static int SamusSpeedX { get; set; } = 0;
+        public static int SamusSpeedY { get; set; } = 0;
+
+        public static FacingDirection SamusFacing { get; set; } = FacingDirection.LEFT;
+
+
+
+        public enum FacingDirection{LEFT, RIGHT};
+
+
+
+        public static int MoveSpeed { get; set; } = 10;
+
+
+        public static double AngleBetweenTwoPoints(Point p1, Point p2)
+        {
+            double radian = Math.Atan2(p1.Y - p2.Y, p1.X - p2.X);
+            double angle = radian * Constants.Rad2Deg;
+            if (angle < 0.0)
+                angle += 360.0;
+            angle = 360 - angle;
+            return angle;
+        }
+
+    }
+}
--- Metroid/MainWindow.xaml.cs
+++ Metroid/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Metroid
 {
@@ -20,9 +21,107 @@ namespace Metroid
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+
+            //ImageDrawing idle = new ImageDrawing();
+            //BitmapImage idleImage = new BitmapImage();
+            //idleImage.BeginInit();
+            //idleImage.UriSource = new Uri(@"\Assets\idle.png", UriKind.Relative);
+            //idleImage.EndInit();
+
+            //player.Source = idleImage;
+
+            this.ResizeMode = ResizeMode.NoResize;
+
+            this.PreviewKeyDown += MainWindow_PreviewKeyDown;
+
         }
+
+        private void MainWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D) //Right
+            {
+                Constants.SamusFacing = Constants.FacingDirection.RIGHT;
+                Constants.SamusPositionX += Constants.MoveSpeed;
+                Constants.State = 0;
+            }
+            if (e.Key == Key.A) //Left
+            {
+                Constants.SamusFacing = Constants.FacingDirection.LEFT;
+                Constants.SamusPositionX -= Constants.MoveSpeed;
+                Constants.State = 0;
+            }
+            if (e.Key == Key.W) //Up
+            {
+
+            }
+            if (e.Key == Key.S) //Down
+            {
+
+            }
+            //throw new NotImplementedException();
+        }
+
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+
+            Point mousePosition = Mouse.GetPosition(this);
+            Console.WriteLine("Mouse at: " + mousePosition);
+
+
+            //Canvas.SetTop(GameCursor, mousePosition.Y - Constants.TILE_HEIGHT / 2);
+            //Canvas.SetLeft(GameCursor, mousePosition.X - Constants.TILE_WIDTH / 2);
+
+            //GameCursor.Margin = new Thickness(mousePosition.X - TILE_WIDTH/2, mousePosition.Y - TILE_HEIGHT/2, 32, 32);
+
+            double angle = Constants.AngleBetweenTwoPoints(new Point(Canvas.GetTop(RightTorso), Canvas.GetLeft(RightTorso)), new Point(Canvas.GetTop(GameCursor), Canvas.GetLeft(GameCursor)));
+
+            Console.WriteLine("Angle from player to cursor: " + angle);
+            RightTorso.RenderTransform = new RotateTransform(angle - 90);
+
+            if ((Constants.State - Constants.State % 10) % 10 == 0)
+            {
+                int x = Constants.State - Constants.State % 10;
+                if (x == 100)
+                {
+                    x = 0;
+                    Constants.State = 0;
+                }
+
+                BitmapSource bs = new BitmapImage(new Uri(@"C:\Users\prosdkr\Desktop\Metroid\Metroid\Assets\leftrun.png"));
+                BitmapSource bs2 = new BitmapImage(new Uri(@"C:\Users\prosdkr\Desktop\Metroid\Metroid\Assets\rightrun.png"));
+
+
+
+
+                CroppedBitmap croppedBitmap = new CroppedBitmap(bs, new Int32Rect((x / Constants.LEFT_RUN_STATES) * Constants.SAMUS_STANDING_WIDTH,
+                                                                                  0,
+                                                                                  Constants.SAMUS_STANDING_WIDTH,
+                                                                                  Constants.SAMUS_STANDING_HEIGHT));
+                CroppedBitmap croppedBitmapR = new CroppedBitmap(bs2, new Int32Rect((x / Constants.LEFT_RUN_STATES) * Constants.SAMUS_STANDING_WIDTH,
+                                                                  0,
+                                                                  Constants.SAMUS_STANDING_WIDTH,
+                                                                  Constants.SAMUS_STANDING_HEIGHT));
+
+                if (Constants.SamusFacing == Constants.FacingDirection.LEFT)
+                    LeftRun.Source = croppedBitmap;
+                if (Constants.SamusFacing == Constants.FacingDirection.RIGHT)
+                    LeftRun.Source = croppedBitmapR;
+
+                Canvas.SetTop(LeftRun, Constants.SamusPositionY);
+                Canvas.SetLeft(LeftRun, Constants.SamusPositionX);
+
+            }
+            Constants.State += 1;
+
+        }
+
+
+
     }
 }
